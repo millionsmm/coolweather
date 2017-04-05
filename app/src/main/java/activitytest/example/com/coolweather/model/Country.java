87bd77ad7d50825dc3b48cebbbd5d49f520a7c69
millionsmm <2415122956@qq.com>
2017-04-05 19:31:35 +0800

    新增数据库帮助类，以及各类对应的实体类。

---
+++ app/src/main/java/activitytest/example/com/coolweather/model/Country.java
@@ -0,0 +1,44 @@
+package activitytest.example.com.coolweather.model;
+
+/**
+ * Created by Administrator on 2017/4/4 0004.
+ */
+
+public class Country {
+    private int id;
+    private String countryName;
+    private String countryCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/activitytest/example/com/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package activitytest.example.com.coolweather.model;
+
+/**
+ * Created by Administrator on 2017/4/4 0004.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
